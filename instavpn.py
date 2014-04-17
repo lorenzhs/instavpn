bc61277e09614fc093e64c3902972a43226024e9
Lorenz Hübschle-Schneider <lorenz-dev@lgh-alumni.de>
2014-04-17 17:06:41 +0200

    Add self-destruction

--- instavpn.py
+++ instavpn.py
@@ -78,6 +78,10 @@ class API(object):
         response = self._get('droplets/new', arguments)
         return response
 
+    def destruct_command(self, id, scrub):
+        return 'https://api.digitalocean.com/droplets/{droplet_id}/destroy/?scrub={scrub}&api_key={api_key}&client_id={client_id}'.format(
+            droplet_id=id, scrub=scrub, api_key=settings.API_KEY, client_id=settings.CLIENT_ID)
+
 class DeploymentModule(object):
     def __init__(self):
         pass
@@ -117,7 +121,7 @@ class InstaVPN(object):
         start_time = time.time()
         droplet = self.api.createDroplet(args.region, args.droplet_name, args.droplet_size, args.image, ssh_keys)
         droplet_id = droplet["droplet"]["id"]
-        print(droplet, droplet_id)
+        print('Droplet creation initiated, id={id}'.format(id=droplet_id))
 
         # Wait 20 seconds to allow DigitalOcean to create the droplet
         time.sleep(20)
@@ -128,7 +132,7 @@ class InstaVPN(object):
             state = self.api.status(droplet_id)
             status = state["droplet"]["status"]
             if status == "new":
-                print("Droplet not yet active:", status, json.dumps(state, indent=4))
+                print("Droplet not yet active, retrying in 5s")
                 time.sleep(5)
                 retries += 1
             elif status == "active":
@@ -145,7 +149,7 @@ class InstaVPN(object):
             # Failed to create droplet
             raise ValueError("failed to create droplet :(")
 
-        print(json.dumps(state, indent=4))
+        print(json.dumps(state))
         return state
 
     def connect(self, status, subnet, proxyDns, additional_args):
@@ -158,19 +162,58 @@ class InstaVPN(object):
         self.deployer.prepare()
         self.deployer.connect(remote, subnet, proxyDns, additional_args)
 
+    def initiate_self_destruct(self, status, self_destruct_timeout, scrub):
+        droplet_id = status["droplet"]["id"]
+        droplet_ip = status["droplet"]["ip_address"]
+        apicall = self.api.destruct_command(droplet_id, scrub)
+        destruct_command = """\"\
+while true
+do
+  while true
+  do
+    if [ \\$(pidof python2) ]
+    then
+      sleep 10
+      echo waiting >> log
+    else
+      echo not found, starting killing >> log
+      break
+    fi
+  done
+  echo waiting for timeout... >> log
+  sleep {timeout}
+  if [ ! \\$(pidof python2) ]
+  then
+    echo not found, killing >> log
+    break
+  fi
+done
+echo curling >> log
+curl \\\"{apicall}\\\" >> log 2>> log
+echo done >> log\"""".format(timeout=self_destruct_timeout, apicall=apicall)
+
+        remote = 'root@' + droplet_ip
+        proc = subprocess.Popen(['ssh', remote, 'nohup', 'bash', '-c', destruct_command])
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Instant sshuttle VPN via DigitalOcean')
+    # sshuttle arguments
     parser.add_argument('-d', '--dns', dest='proxyDns', action='store_true', default=settings.PROXY_DNS, help='Proxy DNS queries through VPN')
     parser.add_argument('-l', '--subnet', type=str, dest='subnet', default=settings.SUBNET, help='Subnet mask to forward through VPN')
     parser.add_argument('-p', '--params', type=str, dest='additional_args', default=settings.ADDITIONAL_ARGS, help='Additional arguments to pass to sshuttle')
+    # Destruction arguments
+    parser.add_argument('--no-self-destruct', dest='self_destruct', action='store_false', default=settings.SELF_DESTRUCT, help='Self destruct droplet after inactivity period')
+    parser.add_argument('--self-destruct-timeout', type=int, dest='self_destruct_timeout', default=settings.SELF_DESTRUCT_TIMEOUT, help='Self destruct interval (seconds)')
+    parser.add_argument('--scrub', dest='scrub', action='store_true', default=settings.SCRUB, help='Scrub droplet after destruction')
+    # Droplet arguments
     parser.add_argument('-r', '--region', type=str, dest='region', default=settings.REGION, help='Region to create the droplet in (slug or id)')
     parser.add_argument('-n', '--name', type=str, dest='droplet_name', default=settings.NAME, help='Name of the droplet')
     parser.add_argument('-s', '--size', type=str, dest='droplet_size', default=settings.SIZE, help='Droplet size (slug or id)')
     parser.add_argument('-i', '--image', type=str, dest='image', default=settings.IMAGE, help='Droplet image (slug or id)')
     parser.add_argument('--all-keys', dest='all_keys', default=False, help='Retrieve and add all keys in your account to the droplet')
     parser.add_argument('-k', '--keys', type=csv, dest='ssh_keys', default=settings.SSH_KEY_IDS, help='SSH key IDs to add to your droplet')
+    # Debug
     parser.add_argument('--debug-state', type=str, dest='debugstate', help='Load droplet state from JSON string')
     args = parser.parse_args()
 
@@ -188,4 +231,7 @@ if __name__ == '__main__':
             ssh_keys = getSshKeys()
 
         status = vpn.createMachine(args, ssh_keys)
-    status = vpn.connect(status, args.proxyDns, args.subnet, args.additional_args)
+    vpn.connect(status, args.proxyDns, args.subnet, args.additional_args)
+    if (args.self_destruct):
+        print("Setting up self-destruct mechanism on droplet...")
+        vpn.initiate_self_destruct(status, args.self_destruct_timeout, args.scrub)
